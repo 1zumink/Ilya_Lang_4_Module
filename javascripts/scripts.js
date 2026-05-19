abfa5241b8979fd130a849712c47f19f6640cd85
1zumink <ilangforgames21@gmail.com>
2026-05-19 13:42:34 +0300

    primal commit

---
+++ javascripts/scripts.js
@@ -0,0 +1,11 @@
+//все скрипты для всех html-страниц
+
+burgerMenu()
+
+function burgerMenu() {
+  let burger = document.querySelector('#burger')
+
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('active')
+  })
+}
