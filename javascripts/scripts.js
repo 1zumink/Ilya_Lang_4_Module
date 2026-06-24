1dbb4ccde4ba35824f18bb638109aa3fd68b9517
1zumink <ilangforgames21@gmail.com>
2026-06-24 05:13:31 +0300

    Купил жижку не спросили паспорт)

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,11 +1,40 @@
 //все скрипты для всех html-страниц
 
 burgerMenu()
+plansScroll()
 
 function burgerMenu() {
   var burger = document.querySelector('#burger')
 
   burger.onclick = function () {
     burger.classList.toggle('active')
+  }
+}
+
+// На адаптиве: раскрываем карточку плана, когда она в середине экрана
+function plansScroll() {
+  var cards = document.querySelectorAll('.planCard')
+  if (!cards.length) {
+    return
+  }
+
+  var observer = new IntersectionObserver(handlePlanCards, {
+    rootMargin: '-45% 0px -45% 0px'
+  })
+
+  var i
+  for (i = 0; i < cards.length; i++) {
+    observer.observe(cards[i])
+  }
+}
+
+function handlePlanCards(entries) {
+  var i
+  for (i = 0; i < entries.length; i++) {
+    if (entries[i].isIntersecting) {
+      entries[i].target.classList.add('planActive')
+    } else {
+      entries[i].target.classList.remove('planActive')
+    }
   }
 }
